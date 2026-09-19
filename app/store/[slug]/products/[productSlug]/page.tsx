d1252832e24f53587c9b0bfad172f7362fb34712
sarusakib <sorowar2466@gmail.com>
2026-09-20 03:05:17 +0600

    fix: wire canonical URL into product sharing

--- app/store/[slug]/products/[productSlug]/page.tsx
+++ app/store/[slug]/products/[productSlug]/page.tsx
@@ -119,7 +119,7 @@ export default async function ProductPage({ params }: Props) {
             <span>{store.name}</span>
           </Link>
           <div className="nav-actions">
-            <ShareProduct title={product.name} />
+            <ShareProduct title={product.name} url={canonical} />
             <Link href={getStoreUrl(store.slug)} className="button">Store</Link>
           </div>
         </header>
