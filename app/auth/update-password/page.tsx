e20fa64550053867a7e5ff0ad56880c21b104d9d
sarusakib <sorowar2466@gmail.com>
2026-09-20 02:56:34 +0600

    feat: complete account authentication flow

---
+++ app/auth/update-password/page.tsx
@@ -0,0 +1,11 @@
+import UpdatePasswordForm from "@/app/auth/update-password/UpdatePasswordForm";
+
+export default function UpdatePasswordPage() {
+  return (
+    <main className="shell auth-shell">
+      <div className="page auth-page">
+        <UpdatePasswordForm />
+      </div>
+    </main>
+  );
+}
